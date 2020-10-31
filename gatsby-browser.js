26df0b7a6d9729eecf917f5c2ed22deb8c11123c
Waleed <22619190+waleed-alfaifi@users.noreply.github.com>
2020-11-01 02:06:56 +0300

    gatsby browser and ssr to commonJS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,22 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-import './src/styles/css/output.css';
-import './src/styles/css/line-awesome.min.css';
-import './src/styles/css/main.css';
+// import './src/styles/css/output.css';
+// import './src/styles/css/line-awesome.min.css';
+// import './src/styles/css/main.css';
+// import React from 'react';
+// import Layout from './src/components/layout';
+// export const wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>;
+// };
 
-import React from 'react';
-import Layout from './src/components/layout';
+require('./src/styles/css/output.css');
+require('./src/styles/css/line-awesome.min.css');
+require('./src/styles/css/main.css');
 
-export const wrapPageElement = ({ element, props }) => {
+const React = require('react');
+const Layout = require('./src/components/layout').default;
+
+exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,8 +6,15 @@
 
 // You can delete this file if you're not using it
 
-import Layout from './src/components/layout';
+// import Layout from './src/components/layout';
 
-export const wrapPageElement = ({ element, props }) => {
+// export const wrapPageElement = ({ element, props }) => {
+//   return <Layout {...props}>{element}</Layout>;
+// };
+
+const React = require('react');
+const Layout = require('./src/components/layout').default;
+
+exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
