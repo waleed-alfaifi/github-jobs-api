c3f207269175969cfa1566d5552330b8a268dc85
Waleed Alfaifi <22619190+waleed-alfaifi@users.noreply.github.com>
2022-01-15 16:42:56 +0300

    Update JobDetailsHeader.js

--- src/components/job_details/JobDetailsHeader.js
+++ src/components/job_details/JobDetailsHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import 'twin.macro';
 
@@ -30,14 +30,15 @@ const Container = styled.div`
 `;
 
 const JobDetailsHeader = ({ job }) => {
+  const placeholderImg = "/company-placeholder.jpg";
+  const [imageUrl, setImageUrl] = useState(job.company_logo || placeholderImg);
+  
   return (
     <Container tw="flex flex-col md:flex-row items-center relative rounded-md md:rounded-bl-lg text-center md:text-left pt-20 px-8 pb-8 md:p-0 mb-4">
       <img
-        src={
-          job.company_logo ||
-          'https://rfqassist.com/wp-content/uploads/2020/03/company-placeholder.png'
-        }
+        src={imageUrl}
         alt={job.title}
+        onError={() => setImageUrl(placeholderImg)}
         tw="md:self-stretch relative -mt-32 mb-8 md:mt-0 md:mb-0 w-20 h-20 md:w-56 md:h-auto bg-white p-2 shadow-xs rounded-3xl md:rounded-none md:rounded-bl-lg object-contain"
       />
 
